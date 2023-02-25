3208fef5057876e7a975be7897d41c16089af511
Kaian Samuel <kaiansamuelster@gmail.com>
2023-02-25 17:27:36 -0300

    Strings em Typescript

--- basico.js
+++ basico.js
@@ -1,2 +1,5 @@
 var a = 'Teste Typescript';
 console.log(a.toUpperCase());
+// strings
+var message = 'Hello World!!!';
+console.log(message);
--- basico.ts
+++ basico.ts
@@ -1,3 +1,8 @@
 const a : string = 'Teste Typescript';
 
 console.log(a.toUpperCase())
+
+// strings
+
+let message = 'Hello World!!!'
+console.log(message)
