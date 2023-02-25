9ef760f6538009e1c63c86eb0f74d1e074272a1f
Kaian Samuel <kaiansamuelster@gmail.com>
2023-02-25 17:34:47 -0300

    Numbers e Boolean em Typescript

--- basico.js
+++ basico.js
@@ -3,3 +3,10 @@ console.log(a.toUpperCase());
 // strings
 var message = 'Hello World!!!';
 console.log(message);
+//numbers
+var numbers = 27;
+numbers = 27.5;
+console.log("O valor de number em Typescript \u00E9: ".concat(numbers));
+//boolean
+var possuiHobie = true;
+console.log("O valor de possuiHobie \u00E9: ".concat(possuiHobie));
--- basico.ts
+++ basico.ts
@@ -6,3 +6,16 @@ console.log(a.toUpperCase())
 
 let message = 'Hello World!!!'
 console.log(message)
+
+//numbers
+
+let numbers = 27
+numbers = 27.5
+console.log(`O valor de number em Typescript é: ${numbers}`)
+
+//boolean
+
+let possuiHobie = true
+console.log(`O valor de possuiHobie é: ${possuiHobie}`)
+
+
